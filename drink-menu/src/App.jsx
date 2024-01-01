6fc83fecc8d41ee39986fb7d14738e9345dae2d4
Jeremy <Taubman33@gmail.com>
2024-01-01 10:48:36 -0500

    adding drinks

--- drink-menu/src/App.jsx
+++ drink-menu/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const drinks = [
+    {title: "The Weatogue",
+    description: },
+    {title: "Gin Max-tini",
+      description: },
+      {title: "Old Fashioned" ,
+        description: },
+        {title: "New Hotness",
+          description: },
+
+          {title: "Parisian Margarita" ,
+            description: },
+            {title: "Long Beach Iced Tea",
+            description: },
+            {title: "Holy Grail",
+          description: },
+          {title: "Farmington River Rum Punch",
+          description: },
+          {title: "Aperol Spritzer",
+          description: },
+          {title: "Kingston Libre",
+          description: },
+          {title: "Sambuca, neat",
+          description: }
+
+
+
+
+  ]
+
+
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="menu">
+        </div>
+       
     </>
   )
 }
